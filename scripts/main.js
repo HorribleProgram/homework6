5651b91d727c771cb89de0383bb93fc78ca69834
horribleprogram <horribleprogrammer.email@gmail.com>
2021-02-01 14:11:05 -0500

    added <ul> wired IDs to each <li>

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,20 @@ searchButton.addEventListener('click', () => {
   const inputValue = searchInput.value;
   console.log(inputValue);
 });
+
+// Binds search_list to the <ul> element.
+const search_list = document.getElementById('history-list');
+
+// Iterates over each <li> in the <ul> and assigns ids to them from [1, 5] inclusive.
+// Needed to save to localStorage
+let count = 1;
+for (let li of search_list.children) {
+  li.setAttribute("id", count++);
+}
+
+// Search History
+let search_history = [];
+
+// if (localStorage.getItem()) {
+  // TODO: get search_history and populate #history-list.
+// }
